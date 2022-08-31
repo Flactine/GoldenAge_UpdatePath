12dea98bb7321f3f3a30a38672dc0d677f00b1ba
Flactine Lee <1716455702@qq.com>
2022-08-31 09:38:49 +0800

    Update GA1_Fix.sql

--- Fix/GA1_Fix.sql
+++ Fix/GA1_Fix.sql
@@ -3,5 +3,5 @@
 -- DateCreated: 8/31/2022 9:33:44 AM
 --------------------------------------------------------------
 UPDATE Projects
-SET PrereqDistrict = 'LOC_LEADER_BWH_ARFOIRE_C4T_NAME'
+SET PrereqDistrict = 'DISTRICT_CITY_CENTER'
 WHERE ProjectType = 'GA_PROJECT_WONDER_VICTORY1' OR (ProjectType = 'GA_PROJECT_WONDER_VICTORY2');
